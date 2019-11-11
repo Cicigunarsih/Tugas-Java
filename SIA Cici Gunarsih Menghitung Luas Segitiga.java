3554d2e7dd3f7cbc5cd14a9a2c18d4782ec5dd34
Cicigunarsih <57163146+Cicigunarsih@users.noreply.github.com>
2019-11-10 21:49:23 -0800

    Add files via upload

---
+++ SIA Cici Gunarsih Menghitung Luas Segitiga.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lsson_at_campus;
+
+/**
+ *
+ * @author USER
+ */
+public class Lsson_At_Campus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double Luas, Tinggi, Alas;
+        Tinggi = 7;
+        Alas = 6;
+        Luas = (Tinggi*Alas)/2;
+        System.out.println("Menghitung Luas Segitiga");
+        System.out.println("Tinggi Segitiga:" + Tinggi);
+        System.out.println("Alas Segitiga:" + Alas);
+        System.out.println("Luas Segitiga:" + Luas);
+    }
+    
+}
